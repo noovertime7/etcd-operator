56c45329959f8da6407fed065e1e29b949105de3
chenteng <cc17854330572@163.com>
2022-10-25 21:17:25 +0800

    feat(system): first commit

---
+++ controllers/etcdcluster_controller.go
@@ -0,0 +1,97 @@
+/*
+Copyright 2022 chenteng.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	appsV1 "k8s.io/api/apps/v1"
+	coreV1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	etcdv1alpha1 "github.com/noovertime7/etcd-operator/api/v1alpha1"
+)
+
+// EtcdClusterReconciler reconciles a EtcdCluster object
+type EtcdClusterReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=etcd.yunxue521.top,resources=etcdclusters,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=etcd.yunxue521.top,resources=etcdclusters/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=etcd.yunxue521.top,resources=etcdclusters/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the EtcdCluster object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
+func (r *EtcdClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	//获取实例
+	var etcdCluster etcdv1alpha1.EtcdCluster
+	if err := r.Client.Get(ctx, req.NamespacedName, &etcdCluster); err != nil {
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+	//创建或者更新对应的statefulSet以及Headless Svc 对象
+	//调协
+	var svc coreV1.Service
+	svc.Name = etcdCluster.Name
+	svc.Namespace = etcdCluster.Namespace
+
+	op, err := ctrl.CreateOrUpdate(ctx, r, &svc, func() error {
+		// 调谐的函数必须在这里面实现，实际上是去瓶装我们的service
+		MutateHeadLessSvc(&etcdCluster, &svc)
+		return controllerutil.SetControllerReference(&etcdCluster, &svc, r.Scheme)
+	})
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	log.Info("CreateOrUpdate Svc", op)
+
+	var sts appsV1.StatefulSet
+	sts.Name = etcdCluster.Name
+	sts.Namespace = etcdCluster.Namespace
+
+	op, err := ctrl.CreateOrUpdate(ctx, r, &sts, func() error {
+		// 调谐的函数必须在这里面实现，实际上是去瓶装我们的service
+		MutateStatefulSet(&etcdCluster, &sts)
+		return controllerutil.SetControllerReference(&etcdCluster, &sts, r.Scheme)
+	})
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	log.Info("CreateOrUpdate StatefulSet", op)
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *EtcdClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&etcdv1alpha1.EtcdCluster{}).
+		Complete(r)
+}
